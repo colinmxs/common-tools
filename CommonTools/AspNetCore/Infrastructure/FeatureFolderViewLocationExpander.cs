38ee94f9f3175bb04f187f6e310338cd4a6da905
Colin Smith <ColinSmith1@boisestate.edu>
2017-07-11 14:43:40 -0600

    init

---
+++ CommonTools/AspNetCore/Infrastructure/FeatureFolderViewLocationExpander.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+using System.Collections.Generic;
+
+namespace AspNetCore.Infrastructure
+{
+    public class FeatureFolderViewLocationExpander : IViewLocationExpander
+    {
+        public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+        {
+            //{2} is area, {1} is controller,{0} is the action
+            string[] locations = new string[]
+            {
+                "Features/{1}/{0}.cshtml",
+                "Features/Shared/{0}.cshtml",
+                "Features/{0}.cshtml"
+            };
+            return locations;
+        }
+
+        public void PopulateValues(ViewLocationExpanderContext context)
+        {
+        }
+    }
+}
